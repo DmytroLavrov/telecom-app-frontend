f21a0a3c65c0334d70da26171dbd795338f2193a
dmytrolavrov <dimalavrov2004.06.16@gmail.com>
2025-02-22 12:20:35 +0200

    Update main.ts for correct ErrorInterceptor integration (#10)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { errorInterceptor } from './app/interceptors/error.interceptor';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...appConfig.providers!,
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: errorInterceptor, multi: true },
+  ],
+}).catch((err) => console.error('Bootstrap error:', err));
